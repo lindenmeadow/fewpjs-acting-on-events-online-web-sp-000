3969bd71315370e30accfa4fb1930f406ffb3ea9
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-24 16:16:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let leftNumbers = dodger.style.right.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
 
-  if (left < 0) {
-    dodger.style.right = `${left + 1}px`;
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`;
   }
 }
