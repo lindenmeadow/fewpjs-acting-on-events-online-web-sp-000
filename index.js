a9be04192877037e5aceda15af11f6778020ea3f
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-24 16:20:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,3 @@ function moveDodgerRight() {
     dodger.style.right = `${right - 1}px`;
   }
 }
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 39) {
-    moveDodgerRight()
-  }
-})
